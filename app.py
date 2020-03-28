5605f072ed62cfbf825e16a9b7b907cc40800020
Suraj Kumar <sksuraj2136@gmail.com>
2020-03-28 12:47:44 +0530

    New endpoints

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def register():
 
 	except Exception as e:
 		raise e
-		return jsonify({'status':500})
+		return jsonify({'id':"failed",'status':500})
 		
 
 @app.route('/api/login',methods=['POST'])
@@ -56,7 +56,7 @@ def login():
 			return jsonify({'id':"user not exists!!","status":404})
 	except Exception as e:
 		raise e
-		return jsonify({'status':500})
+		return jsonify({'id':"failed",'status':500})
 
 @app.route('/api/generate_pass',methods=['POST'])
 def generate_pass():
@@ -83,21 +83,60 @@ def generate_pass():
 
 	except Exception as e:
 		raise e
-		return jsonify({'status':500})
+		return jsonify({'id':"failed",'status':500})
 
 #user usecases ends
 
 
 #police usecases starts
-@app.route('/api/get_passes/<status>',methods=['GET'])
-def get_passes(status):
-	passes = mongo.db.passes
-	all_passes=[]
-	for p in passes.find({"status":int(status)}):
-		p['_id']=str(p['_id'])
-		all_passes.append(p)
-	return jsonify(all_passes)
 
+@app.route('/api/police/get_passes/<status>',methods=['GET'])
+def get_passes(status):
+	try:
+		passes = mongo.db.passes
+		all_passes=[]
+		for p in passes.find({"status":int(status)}):
+			p['_id']=str(p['_id'])
+			all_passes.append(p)
+		return jsonify(all_passes)
+	except Exception as e:
+		raise e
+		return jsonify({'id':"failed",'status':500})
+
+
+@app.route('/api/police/validate_pass',methods=['PUT'])
+def validate_pass():
+	try:
+		passes = mongo.db.passes
+
+		passes.find_one_and_update({"_id":ObjectId(request.json['pid'])},
+			{'$set':{'status':int(request.json['status'])}})
+
+		#implement notification here
+
+		return jsonify({'id':"updated",'status':200})
+	except Exception as e:
+		raise e
+		return jsonify({'id':"failed",'status':500})
+
+
+@app.route('/api/police/get_user/<id>',methods=['GET'])
+def get_user(id):
+	try:
+		users = mongo.db.user
+
+		user = users.find_one({"_id":ObjectId(id)})
+		if user:
+			user['_id']= str(user['_id'])
+			del user['password']
+			return jsonify(user)
+		else:
+			return jsonify({'id':"user not exists!!","status":404})
+
+	except Exception as e:
+		raise e
+		return jsonify({'id':"failed",'status':500})
+		
 #police usecases ends
 
 if __name__ == '__main__':
